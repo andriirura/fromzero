69436c2981ce97879102857f025620970b4cc8f3
Nazar Kalashnikov <nazar.kalashnikov@gmail.com>
2021-07-31 16:47:56 +0300

    Switch to NUnit framework

--- TestProject1/LeetCodeTests.cs
+++ TestProject1/LeetCodeTests.cs
@@ -1,16 +1,19 @@
-using Microsoft.VisualStudio.TestTools.UnitTesting;
 using AndrewFromZeroToHero;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using NUnit.Framework;
+using Assert = Microsoft.VisualStudio.TestTools.UnitTesting.Assert;
+using CollectionAssert = NUnit.Framework.CollectionAssert;
 using Program = AndrewFromZeroToHero.Program;
 
 namespace TestProject1
 {
-    [TestClass]
+    [TestFixture]
     public class LeetCodeTests
     {
-        [TestMethod]
-        [DataRow(3, 12, 15)]
-        [DataRow(0, 0, 0)]
-        [DataRow(2, 2, 5)]
+        [Test]
+        [TestCase(3, 12, 15)]
+        [TestCase(0, 0, 0)]
+        [TestCase(2, 2, 5)]
         public void SumTest(int first, int second, int expectedResult)
         {
             int result = Program.Sum(first, second);
@@ -19,11 +22,11 @@ namespace TestProject1
         }
 
 
-        [TestMethod]
-        [DataRow(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
-        [DataRow(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
-        [DataRow(new[] { 3, 3 }, 6, new[] { 0, 1 })]
-        [DataRow(new[] { 5,8, 2, 0,14,4,9 }, 11, new[] { 2, 6 })]
+        [Test]
+        [TestCase(new[] { 2, 7, 11, 15 }, 9, new[] { 0, 1 })]
+        [TestCase(new[] { 3, 2, 4 }, 6, new[] { 1, 2 })]
+        [TestCase(new[] { 3, 3 }, 6, new[] { 0, 1 })]
+        [TestCase(new[] { 5,8, 2, 0,14,4,9 }, 11, new[] { 2, 6 })]
         public void LC1_TwoSum_Test(int[] numbers, int target, int[] expectedResult)
         {
             int[] result = LeetcodeTasks.LC1_TwoSum(numbers, target);
