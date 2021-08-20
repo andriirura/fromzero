c5bf17e282a9932e7465f29772414d87cfa376a4
andrii.rura <andrii.rura@gmail.com>
2021-08-21 00:28:45 +0300

    Ceasar code

---
+++ Ceasar/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Globalization;
+
+namespace Ceasar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter the text: ");
+            string text = Convert.ToString(Console.ReadLine());
+            Console.Write("Enter the key: ");
+            int key = Convert.ToInt32(Console.ReadLine());
+            int number;
+            string code = string.Empty;
+            char[] abc =
+            {
+                'a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u',
+                'v', 'w', 'x', 'y', 'z'
+            };
+            for (int i = 0; i < text.Length; i++)
+            {
+                int a;
+                char item = text[i];
+                a = abc.IndexOf(item);
+                number = a + key;
+                code = code + abc[number];
+
+            }
+            Console.Write($"{code} ");
+
+        }
+        }
+    }
+
--- CodeTester/Program.cs
+++ CodeTester/Program.cs
@@ -1,15 +1,31 @@
 ﻿using System;
 
-namespace CodeTester
+namespace Shifr
 {
     class Program
     {
         static void Main(string[] args)
         {
-            int range = 5;
-            int i = 10;
-            int f = i % range + 3;
-            Console.WriteLine(f);
+            Console.WriteLine("Введите слово,которое нужно зашифровать:");
+            string s = Console.ReadLine();
+            string abc = "abcdefghijklmnopqrstuvwxyz";
+            //abc наш алфавит
+            Console.WriteLine("Укажите длину сдвига");
+            int len = Convert.ToInt32(Console.ReadLine());
+            // len - число сдвигов в сторону
+            int a;
+            string code = string.Empty;
+            for (int i = 0; i < s.Length; i++)
+            {
+                int pos;
+                char c = s[i];
+                pos = abc.IndexOf(c);
+                a = pos + len;
+                code += abc[a];
+            }
+
+            Console.WriteLine(code);
+            Console.ReadLine();
         }
     }
 }
