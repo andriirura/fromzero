93dd2aff8f008360a6f3540e9b641bddd5fa8790
andrii.rura <andrii.rura@gmail.com>
2021-08-21 14:37:35 +0300

    Class + Card(Rank,Suite)

--- CodeTester/Program.cs
+++ CodeTester/Program.cs
@@ -1,31 +1,54 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace Shifr
 {
+    enum Suite
+    {
+        Diamonds,
+        Hearts,
+        Clubs,
+        Spades
+    }
+
+    enum Rank
+    {
+        Ace,
+        King,
+        Queen,
+        Jack
+    }
+
+    class Card
+    {
+        public Rank Rank;
+        public Suite Suite;
+        public override string ToString()
+        {
+            string result= $"{Rank} {Suite}";
+            return result;
+            //return Rank.ToString();
+        }
+    }
+
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Введите слово,которое нужно зашифровать:");
-            string s = Console.ReadLine();
-            string abc = "abcdefghijklmnopqrstuvwxyz";
-            //abc наш алфавит
-            Console.WriteLine("Укажите длину сдвига");
-            int len = Convert.ToInt32(Console.ReadLine());
-            // len - число сдвигов в сторону
-            int a;
-            string code = string.Empty;
-            for (int i = 0; i < s.Length; i++)
-            {
-                int pos;
-                char c = s[i];
-                pos = abc.IndexOf(c);
-                a = pos + len;
-                code += abc[a];
-            }
+            Card a = new Card();
+            a.Suite = Suite.Spades;
+            a.Rank = Rank.Queen;
+            Card b = new Card();
+            b.Suite = Suite.Clubs;
+            b.Rank = Rank.King;
 
-            Console.WriteLine(code);
-            Console.ReadLine();
+            List<Card> list = new List<Card>();
+            list.Add(a);
+            list.Add(b);
+            for (int i = 0; i < list.Count; i++)
+            {
+                Console.WriteLine(list[i]);
+            }
         }
     }
 }
