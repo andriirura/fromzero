d22a9654b77dbe63024ab3fc9321a1d7b4d86eb9
andrii.rura <andrii.rura@gmail.com>
2021-08-24 00:43:52 +0300

    ConsignmentShop

---
+++ ConsignmentShopLibrary/Store.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using ConsignmentShopUI;
+
+namespace ConsignmentShopLibrary
+{
+    public class Store
+    {
+        public string Name { get; set; }
+        public List<Vendor> Vendors { get; set; }
+        public List<Item> Items { get; set; }
+
+        public Store()
+        {
+            Vendors = new List<Vendor>();
+            Items = new List<Item>();
+        }
+    }
+}
---
+++ ConsignmentShopLibrary/Vendor.cs
@@ -0,0 +1,24 @@
+﻿namespace ConsignmentShopLibrary
+{
+    public class Vendor
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public double Commission { get; set; }
+
+        public decimal PaymentDue { get; set; }
+
+        public string Display
+        {
+            get
+            {
+                return string.Format("{0} {1} - ${2}", FirstName, LastName, PaymentDue);
+            }
+        }
+
+        public Vendor()
+        {
+            Commission = .5;
+        }
+    }
+}
