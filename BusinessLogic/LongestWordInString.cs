4243f023a1c9b80236fd8d6940a13ce729346e87
andrii.rura <andrii.rura@gmail.com>
2021-09-04 21:36:28 +0300

    LongestWordInString + tests

---
+++ BusinessLogic/LongestWordInString.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BusinessLogic
+{
+    public class LongestWordInString
+    {
+        public static string GetLongestWord(string text)
+        {
+            
+
+            // split string into words
+            var words = new List<string>();
+            string max = "";
+            while (text.Length > 0)
+            {
+                int indexOfSpace = text.IndexOf(' ');
+
+                if (indexOfSpace == -1)
+                {
+                    words.Add(text);
+                    text = "";
+                }
+                else
+                {
+                    string word = text.Substring(0, indexOfSpace);
+                    words.Add(word);
+                    text = text.Substring(indexOfSpace + 1);
+                }
+            }
+
+            // find longest word
+            foreach ( string item in words)
+            {
+                if (item.Length > max.Length)
+                {
+                    max = item;
+                }
+            }
+
+            return max;
+        }
+    }
+}
---
+++ TestProject1/LongestWordTests.cs
@@ -0,0 +1,44 @@
+﻿using BusinessLogic;
+using NUnit.Framework;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using FluentAssertions;
+
+namespace TestProject1
+{
+    [TestFixture]
+    public class LongestWordTests
+    {
+        [Test]
+        [TestCase("Write a C# Sharp Program to display the following pattern using the alphabet.", "following")]
+        [TestCase("A developer platform for building all your apps.", "developer")]
+        public void LongestWordTest(string text, string expectedResult)
+        {
+            string result = LongestWordInString.GetLongestWord(text);
+
+            result.Should().Be(expectedResult);
+        }
+
+
+        [Test]
+        public void TestiTest()
+        {
+            List<int> numbers = new List<int>();
+
+            int max = 0;// numbers[0];
+
+            foreach (int item in numbers)
+            {
+                if (item > max )
+                {
+                    max = item;
+                }
+            }
+            Console.WriteLine(max);
+        }
+
+    }
+}
